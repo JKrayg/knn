e0281f600d0f87b6f949218b8f535966c090c137
jkrayg <jk1019602@gmail.com>
2025-02-01 19:46:19 -0500

    fix to keep integrity of origin data. still ugly. still need to break ties and scale by feature

--- knn.py
+++ knn.py
@@ -1,34 +1,30 @@
-# k-Nearest Neigbor
+# Jake Krayger
+# k-Nearest Neighbor
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-import random
 from sklearn.datasets import load_iris
 
 
 k = 10
-numPoints = 20
-data = []
-dimensions = 2
-dataCount = 1
-
+dimensions = 3
 
 iris = load_iris()
 data = np.array(iris["data"])
 
-
-# data
+# 2D
 # data = [([1, 1], 0), ([2, 1], 0), ([1.5, 2], 0), ([2, 2], 0), ([1, 3], 0),
 # ([3, 1], 0), ([1.5, 1.5], 0), ([2.5, 2], 0), ([1.8, 2.8], 0), ([1, 2.5], 0),
 # ([4, 4], 1), ([5, 5], 1), ([4.5, 5], 1), ([5, 4.5], 1), ([6, 6], 1),
 # ([3.5, 4.2], 1), ([4.8, 4.8], 1), ([5.2, 5.3], 1), ([6.5, 6.5], 1), ([5, 6], 1)]
 
+# 3D
 data = [([1, 1, 1], 0), ([2, 1, 1], 0), ([1.5, 2, 1], 0), ([2, 2, 1], 0), ([1, 3, 1], 0),
 ([3, 1, 1], 0), ([1.5, 1.5, 1], 0), ([2.5, 2, 1], 0), ([1.8, 2.8, 1], 0), ([1, 2.5, 1], 0),
 ([4, 4, 6.5], 1), ([5, 5, 6.5], 1), ([4.5, 5, 6.5], 1), ([5, 4.5, 6.5], 1), ([6, 6, 6.5], 1),
 ([3.5, 4.2, 6.5], 1), ([4.8, 4.8, 6.5], 1), ([5.2, 5.3, 6.5], 1), ([6.5, 6.5, 6.5], 1), ([5, 6, 6.5], 1)]
 
-test_point = [[1.95, 5, 4]]
+test_point = [3.2, 3.2, 3.5]
 
 points = []
 labels = []
@@ -38,14 +34,9 @@ def splitData(d):
         points.append(i[0])
         labels.append(i[1])
 
-splitData(data)
-
+# need to scale by feature not just greatest value
 def scale(d):
     return [i / max(np.array(points).flatten()) for i in [j for j in d]]
-
-train_data = list(zip(scale(points), labels))
-test_point = scale(test_point)
-# print(train_data)
 
 
 # euclidean distance
@@ -56,11 +47,11 @@ def euc(point1, point2):
     return math.sqrt(dis)
 
 
-
+# need to break ties and clean this
 def nearestNeighbor(k, point, data):
     dists = []
     for i in data:
-        d = euc(point[0], i[0])
+        d = euc(point, i[0])
         dists.append((i, d))
     
     dists = sorted(dists, key = lambda x: x[1])[:k]
@@ -80,32 +71,27 @@ def nearestNeighbor(k, point, data):
             mex = counts.get(l)
             max_ = l
     
-    print(max_)
-
-    train_data.append(point)
-    labels.append(max_)
-    # print(train_data)
-    # print(labels)
-
-    
-    return min(dists, key = lambda x: x[1])[0]
+    return (point, max_)
 
 
+def plot():
+    t = np.array([i[0] for i in scale_data])
+    if dimensions == 3:
+        t = np.multiply(t, 125)
+        plt.scatter(t[:, 0], t[:, 1], t[:, 2], c=labels, alpha = 0.5, edgecolors='black')
+        plt.scatter(t[len(t) - 1][0], t[len(t) - 1][1], c='red', marker='x')
+    else:
+        plt.scatter(t[:, 0], t[:, 1], c=labels, alpha = 0.5, edgecolors='black')
+        plt.scatter(t[len(t) - 1][0], t[len(t) - 1][1], c='red', marker='x')
 
-# print(k)
-nn = nearestNeighbor(7, test_point, train_data)
-# print(nn)
-t = np.array([i[0] for i in train_data])
-# print(t)
+    plt.show()
 
-# plt.scatter(t[:, 0], t[:, 1], c=labels, alpha = 0.5, edgecolors='black')
-# plt.scatter(test_point[0][0], test_point[0][1], c='red', marker='x')
-# # plt.scatter(nn[0][0], nn[0][1], c='green', marker='x')
-# plt.show()
 
-t = np.multiply(t, 125)
-test_point = np.multiply(test_point, 125)
-plt.scatter(t[:, 0], t[:, 1], t[:, 2], c=labels, alpha = 0.5, edgecolors='black')
-plt.scatter(test_point[0][0], test_point[0][1], c='red', marker='x')
-# plt.scatter(nn[0][0], nn[0][1], c='green', marker='x')
-plt.show()
+nn = nearestNeighbor(5, test_point, data)
+add_pred = data.copy()
+add_pred.append(nn)
+
+splitData(add_pred)
+scale_data = list(zip(scale(points), labels))
+
+plot()
