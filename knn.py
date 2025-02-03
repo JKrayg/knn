a7cf7e8fc3ed8efd5de09903e15f8379b6123557
jkrayg <jk1019602@gmail.com>
2025-02-03 12:23:57 -0500

    scale by feature

--- knn.py
+++ knn.py
@@ -6,37 +6,53 @@ import math
 from sklearn.datasets import load_iris
 
 
-k = 10
-dimensions = 3
+k = 7
 
 iris = load_iris()
 data = np.array(iris["data"])
+labels = np.array(iris["target"])
 
 # 2D
-# data = [([1, 1], 0), ([2, 1], 0), ([1.5, 2], 0), ([2, 2], 0), ([1, 3], 0),
-# ([3, 1], 0), ([1.5, 1.5], 0), ([2.5, 2], 0), ([1.8, 2.8], 0), ([1, 2.5], 0),
-# ([4, 4], 1), ([5, 5], 1), ([4.5, 5], 1), ([5, 4.5], 1), ([6, 6], 1),
-# ([3.5, 4.2], 1), ([4.8, 4.8], 1), ([5.2, 5.3], 1), ([6.5, 6.5], 1), ([5, 6], 1)]
+# data = [[1, 1], [2, 1], [1.5, 2], [2, 2], [1, 3],
+# [3, 1], [1.5, 1.5], [2.5, 2], [1.8, 2.8], [1, 2.5],
+# [4, 4], [5, 5], [4.5, 5], [5, 4.5], [6, 6], [3.5, 4.2],
+# [4.8, 4.8], [5.2, 5.3], [6.5, 6.5], [5, 6]]
 
 # 3D
-data = [([1, 1, 1], 0), ([2, 1, 1], 0), ([1.5, 2, 1], 0), ([2, 2, 1], 0), ([1, 3, 1], 0),
-([3, 1, 1], 0), ([1.5, 1.5, 1], 0), ([2.5, 2, 1], 0), ([1.8, 2.8, 1], 0), ([1, 2.5, 1], 0),
-([4, 4, 6.5], 1), ([5, 5, 6.5], 1), ([4.5, 5, 6.5], 1), ([5, 4.5, 6.5], 1), ([6, 6, 6.5], 1),
-([3.5, 4.2, 6.5], 1), ([4.8, 4.8, 6.5], 1), ([5.2, 5.3, 6.5], 1), ([6.5, 6.5, 6.5], 1), ([5, 6, 6.5], 1)]
+# data = [[1, 1, 1], [2, 1, 1], [1.5, 2, 1], [2, 2, 1], [1, 3, 1],
+# [3, 1, 1], [1.5, 1.5, 1], [2.5, 2, 1], [1.8, 2.8, 1], [1, 2.5, 1],
+# [4, 4, 6.5], [5, 5, 6.5], [4.5, 5, 6.5], [5, 4.5, 6.5], [3.5, 3.5, 3.5],
+# [3.5, 4.2, 6.5], [4.8, 4.8, 6.5], [5.2, 5.3, 6.5], [3, 6.5, 6.5], [5, 6, 6.5]]
 
-test_point = [3.2, 3.2, 3.5]
+# labels = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
-points = []
-labels = []
+test_point = [7, 4, 5.99, 6]
 
 def splitData(d):
+    points = []
+    labels = []
     for i in d:
         points.append(i[0])
         labels.append(i[1])
+    
+    return (points, labels)
 
-# need to scale by feature not just greatest value
-def scale(d):
-    return [i / max(np.array(points).flatten()) for i in [j for j in d]]
+#
+def scale(data, test_point):
+    maxByFeat = [max([j[z] for j in [i for i in data]]) for z in range(len(data[0]))]
+    scaled_data = []
+    scaled_testPoint = []
+
+    for m in data:
+        temp = []
+        for t in range(len(maxByFeat)):
+            temp.append(m[t] / maxByFeat[t])
+        scaled_data.append(np.array(temp))
+    
+    for b in range(len(maxByFeat)):
+        scaled_testPoint.append(test_point[b] / maxByFeat[b])
+
+    return (scaled_data, scaled_testPoint)
 
 
 # euclidean distance
@@ -65,7 +81,7 @@ def nearestNeighbor(k, point, data):
     
 
     mex = 0
-    max_ = 0
+    max_ = 4
     for l in counts:
         if counts.get(l) > mex:
             mex = counts.get(l)
@@ -74,24 +90,26 @@ def nearestNeighbor(k, point, data):
     return (point, max_)
 
 
-def plot():
-    t = np.array([i[0] for i in scale_data])
+def plot(d):
+    t = np.array([i for i in d[0]])
     if dimensions == 3:
         t = np.multiply(t, 125)
-        plt.scatter(t[:, 0], t[:, 1], t[:, 2], c=labels, alpha = 0.5, edgecolors='black')
+        print(t)
+        plt.scatter(t[:, 0], t[:, 1], t[:, 2], c=d[1], alpha = 0.5, edgecolors='black')
         plt.scatter(t[len(t) - 1][0], t[len(t) - 1][1], c='red', marker='x')
     else:
-        plt.scatter(t[:, 0], t[:, 1], c=labels, alpha = 0.5, edgecolors='black')
+        plt.scatter(t[:, 0], t[:, 1], c=d[1], alpha = 0.5, edgecolors='black')
         plt.scatter(t[len(t) - 1][0], t[len(t) - 1][1], c='red', marker='x')
 
     plt.show()
 
 
-nn = nearestNeighbor(5, test_point, data)
-add_pred = data.copy()
+scale_data = scale(data, test_point)
+train_data = list(zip(scale_data[0], labels))
+
+nn = nearestNeighbor(k, scale_data[1], train_data)
+add_pred = train_data.copy()
 add_pred.append(nn)
 
-splitData(add_pred)
-scale_data = list(zip(scale(points), labels))
-
-plot()
+dimensions = len(scale_data[1])
+plot(splitData(add_pred))
